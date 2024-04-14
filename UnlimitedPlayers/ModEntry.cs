e5273676e6d4cf0d26f6fa22d1508b04791a22f6
Jan Brodersen <janbrodersen@armitxes.net>
2024-04-14 19:36:32 +0200

    [ADD] Bank Commands (#25)

--- UnlimitedPlayers/ModEntry.cs
+++ UnlimitedPlayers/ModEntry.cs
@@ -1,4 +1,5 @@
 ﻿using StardewModdingAPI;
+using UnlimitedPlayers.Commands;
 using UnlimitedPlayers.Events.Display;
 using UnlimitedPlayers.Events.GameLoop;
 using UnlimitedPlayers.Events.Multiplayer;
@@ -13,18 +14,25 @@ namespace UnlimitedPlayers
       parser.Store();                // Now we can access the config from every class without helper or passing the instance
       LazyHelper.ModHelper = helper; // And here I am just absolutly lazy - terribly sorry >.<
       LazyHelper.ModEntry = this;    // There will always only be one valid instance + read above
-      RegisterEvents(helper);
+      RegisterEvents();
+      RegisterCommands();
       LazyHelper.LogInfo("Default player limit set to " + LazyHelper.PlayerLimit + " players.");
     }
 
-    public static void RegisterEvents(IModHelper helper)
+    public static void RegisterEvents()
     {
+      IModHelper helper = LazyHelper.ModHelper;
       MenuEvents menuEvents = new();
       helper.Events.GameLoop.GameLaunched += new TickEvents().FirstUpdateTick;
       helper.Events.GameLoop.DayStarted += new DayEvents().DayStarted;
       helper.Events.Display.MenuChanged += menuEvents.MenuChanged;
       helper.Events.Display.RenderingActiveMenu += menuEvents.RenderingActiveMenu;
       helper.Events.Multiplayer.PeerConnected += new ConnectionEvents().PeerConnected;
+    }
+
+    public static void RegisterCommands()
+    {
+      _ = new BankCommand();
     }
   }
 }
